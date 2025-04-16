725edeff66dab2cff07389ff747a4a9fe512f7d6
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-16 15:17:31 +0200

    deploy working?

--- app/api/redirection/route.ts
+++ app/api/redirection/route.ts
@@ -1,13 +1,8 @@
 import { NextResponse } from "next/server";
-
-interface Redirecting {
-  id: number;
-  html_url: string;
-  redirect: string | null;
-}
+import { readFile } from "fs/promises";
+import path from "path";
 
 export async function GET(request: Request) {
-
   const { searchParams } = new URL(request.url);
   const user = searchParams.get("user") || "weuritz8u";
   const redirect = searchParams.get("redirect") || (searchParams.has("redirect") ? null : searchParams.get("r")) || "no";
@@ -15,27 +10,30 @@ export async function GET(request: Request) {
   if (redirect !== "no") {
     let do_redirect = true;
     let redirectUrl: URL | null = null;
-  
+
     if (redirect === "home") {
       redirectUrl = new URL("https://shadowdara.github.io", request.url);
     } else if (redirect === "github") {
       redirectUrl = new URL("https://github.com/shadowdara", request.url);
+    } else if (redirect === "github2") {
+      redirectUrl = new URL("https://github.com/weuritz8u", request.url);
     } else {
       do_redirect = false;
     }
-  
+
     if (do_redirect && redirectUrl) {
       return NextResponse.redirect(redirectUrl.toString(), 302);
     }
   }
-  
 
-  const response = new NextResponse("Redirecting ...\n\nYou should not see this!", {
+  // Absoluter Pfad zur Datei im Projekt (z. B. /public/data/redirection.txt)
+  const filePath = path.join(process.cwd(), "public/data/redirection.txt");
+  const text = await readFile(filePath, "utf-8");
+
+  return new NextResponse(text, {
     status: 200,
     headers: {
       "Content-Type": "text/plain",
     },
   });
-
-  return response;
 }
