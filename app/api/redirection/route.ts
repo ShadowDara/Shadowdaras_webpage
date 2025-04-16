fced1f4f87ce491d40990182be48d4a3e9d5a31c
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-16 16:45:52 +0200

    save

--- app/api/redirection/route.ts
+++ app/api/redirection/route.ts
@@ -3,8 +3,7 @@ import { readFile } from "fs/promises";
 import path from "path";
 
 const homepage = "https://shadowdara.vercel.app"
-const filePath = path.join(process.cwd(), "public/data/redirection.txt");
-const filePath_html = path.join(process.cwd(), "public/data/redirection.html");
+const filePath_html = path.join(process.cwd(), "public/data/redirection.txt");
 
 function wait(seconds: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
@@ -13,17 +12,9 @@ function wait(seconds: number): Promise<void> {
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const user = searchParams.get("user") || "weuritz8u";
-  const redirect = searchParams.get("redirect") || (searchParams.has("redirect") ? null : searchParams.get("r")) || "yes";
+  const redirect = searchParams.get("redirect") || (searchParams.has("redirect") ? null : searchParams.get("r")) || "no";
 
-  if (redirect === "yes") {
-    const text_html = await readFile(filePath_html, "utf-8");
-    return new NextResponse(text_html, {
-      status: 200,
-      headers: { "Content-Type": "text/html" },
-    });
-  }
-
-  else {
+  if (redirect !== "no") {
     let do_redirect = true;
     let redirectUrl: URL | null = null;
 
@@ -36,10 +27,6 @@ export async function GET(request: Request) {
     else if (redirect === "github2") {
       redirectUrl = new URL("https://github.com/weuritz8u", request.url);
     }
-    else if (redirect === "yes") {
-      await wait(10);
-      redirectUrl = new URL(homepage, request.url);
-    }
     else {
       do_redirect = false;
     }
@@ -49,12 +36,9 @@ export async function GET(request: Request) {
     }
   }
 
-  const text = await readFile(filePath, "utf-8");
-
-  return new NextResponse(text, {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain",
-    },
+  const text_html = await readFile(filePath_html, "utf-8");
+  return new NextResponse(text_html, {
+      status: 200,
+      headers: { "Content-Type": "text/plain" },
   });
 }
